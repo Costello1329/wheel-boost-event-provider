1a207b90b86f1b98ea09bdbcfa5323da7f34d33b
Kirill <semennikov1360@mail.ru>
2020-03-15 17:11:22 +0300

    bug fix

--- provider.py
+++ provider.py
@@ -1,52 +1,57 @@
 import sys
 import pandas as pandas
-from pip._vendor import requests
+import http.client
+import json
 
 
 def set_requests_body(db):
     out_dict = {}
-    
+
     for index, row in db.iterrows():
-        out_dict.update(
-            title=row["title"],
-            description=row["description"],
-            coordinates=row["coordinates"],
-            startTime=row["startTime"],
-            endTime=row["endTime"],
-            price=row["price"],
-            peopleCount=row["peopleCount"]
-        )
-
+        event = {
+            "title": row["title"],
+            "coordinates": row["coordinates"],
+            "description": row["description"],
+            "startTime": row["startTime"],
+            "endTime": row["endTime"],
+            "price": row["price"],
+            "peopleCount": row["peopleCount"]
+        }
+        out_dict.update(event)
     body = {
-        "events": out_dict
+        "events": [out_dict]
     }
-
     return body
 
 
-def set_response(url, body):
+def set_response(url, url_path, body):
     try:
-        respose = requests.post(url=url, data=body)
-        print(response)
-    
+        connection = http.client.HTTPConnection(url)
+        json_data = json.dumps(body)
+        headers = {'Content-type': 'application/json'}
+        connection.request('POST', url_path, json_data, headers)
+        response = connection.getresponse()
+        print(response.read().decode())
+        print("success")
     except Exception as description:
         print(description)
 
 
 def main():
-    if len(sys.argv) < 3:
+    if len(sys.argv) < 4:
         print("Error. Too few parameters.")
         sys.exit(1)
 
-    if len(sys.argv) > 3:
+    if len(sys.argv) > 4:
         print("Error. Too many parameters.")
         sys.exit(1)
 
     path = sys.argv[1]
     url = sys.argv[2]
+    url_path = sys.argv[3]
     events = pandas.read_excel(path)
     body = set_requests_body(events)
-    set_response(url, body)
+    set_response(url, url_path, body)
 
 
 if __name__ == "__main__":
