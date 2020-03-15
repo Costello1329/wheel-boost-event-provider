a66d8f1b653dcca32e3264faba8217e01dc14910
Konstantin Leladze <leladze.kg@phystech.edu>
2020-03-15 03:51:39 +0300

    some fixs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WheelBoostEventProvider
 An event provider for WheelBoostBackend module, that parses .csv files and sends request to add event to backend’s database.
+
+### How to use:
+Run this command `python3 provider.py __path__ __host__`, where `__path__` is the path to your .xlsx file with data, and `__host__` is the WheelBoostBackend API instance.
--- provider.py
+++ provider.py
@@ -1,11 +1,11 @@
 import sys
-
 import pandas as pandas
 from pip._vendor import requests
 
 
 def set_requests_body(db):
     out_dict = {}
+    
     for index, row in db.iterrows():
         out_dict.update(
             title=row["title"],
@@ -14,32 +14,40 @@ def set_requests_body(db):
             startTime=row["startTime"],
             endTime=row["endTime"],
             price=row["price"],
-            peopleCount=row["peopleCount"])
+            peopleCount=row["peopleCount"]
+        )
 
     body = {
         "events": out_dict
     }
+
     return body
 
 
 def set_response(url, body):
     try:
-        r = requests.post(url=url, data=body)
-        print(r)
-    except Exception as s:
-        print(s)
+        respose = requests.post(url=url, data=body)
+        print(response)
+    
+    except Exception as description:
+        print(description)
 
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) < 3:
-        print("Ошибка. Слишком мало параметров.")
+        print("Error. Too few parameters.")
         sys.exit(1)
 
     if len(sys.argv) > 3:
-        print("Ошибка. Слишком много параметров.")
+        print("Error. Too many parameters.")
         sys.exit(1)
+
     path = sys.argv[1]
     url = sys.argv[2]
     events = pandas.read_excel(path)
     body = set_requests_body(events)
     set_response(url, body)
+
+
+if __name__ == "__main__":
+    main()
